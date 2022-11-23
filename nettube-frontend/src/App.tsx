ed56c363178236cd05e9c4fe3bdfbcbad1067f75
Jakub Filipowski <filipowski.kuba@gmail.com>
2022-11-23 23:01:09 +0100

    feat(frontend): add mapping and package.json fixes

--- nettube-frontend/src/App.tsx
+++ nettube-frontend/src/App.tsx
@@ -1,99 +1,60 @@
-import './App.css';
-import MediaCard from './components/MediaCard'
-import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
-import ResponsiveAppBar from './components/ResponsiveAppBar';
-import SearchBar from './components/SearchBar';
-import Carousel from './components/Carousel';
+import "./App.css";
+import MediaCard from "./components/MediaCard";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import SearchBar from "./components/SearchBar";
+import Carousel from "./components/Carousel";
 
 export type TestCardProps = {
-  title: string,
-  alt: string,
-  description: string,
-  thumbnail: string
-}
-
-export type SelectedVideos = {
-  videos: Array<string>;
-} 
+	title: string;
+	alt: string;
+	description: string;
+	thumbnail: string;
+};
 
 const TestCard: TestCardProps = {
-  title: "Testowy",
-  alt: "alter",
-  thumbnail: "bleach.png",
-  description: "testowy description"
-}
+	title: "Testowy",
+	alt: "alter",
+	thumbnail: "bleach.png",
+	description: "testowy description",
+};
 const TestCard2: TestCardProps = {
-  title: "Testowy2",
-  alt: "alter2",
-  thumbnail: "",
-  description: "testowy description2"
+	title: "Testowy2",
+	alt: "alter2",
+	thumbnail: "",
+	description: "testowy description2",
+};
+
+const DUMMY:TestCardProps[] = [];
+for (let i = 0; i < 10; i++) {
+	DUMMY.push(TestCard);
 }
 
-const images: SelectedVideos = {
-  videos:  ['https://placeimg.com/640/480/animals', 'https://placeimg.com/640/480/nature', 'https://placeimg.com/640/480/architecture']
-}
-const videos = ['https://placeimg.com/640/480/animals', 'https://placeimg.com/640/480/nature', 'https://placeimg.com/640/480/architecture'];
+const links = [
+	"https://placeimg.com/640/480/animals",
+	"https://placeimg.com/640/480/nature",
+	"https://placeimg.com/640/480/architecture",
+];
 
 function App() {
-
-  return (
-    <><nav>
-      <ResponsiveAppBar />
-    </nav><div className='wrapping'>
-      <SearchBar/>
-      <Carousel {...images}/>
-        <Grid2 container spacing={2}>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard2} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard2} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard2} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard2} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard2} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard2} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard2} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard} />
-          </Grid2>
-          <Grid2 xs={3}>
-            <MediaCard {...TestCard2} />
-          </Grid2>
-        </Grid2>
-      </div></>
-  );
+	return (
+		<>
+			<nav>
+				<ResponsiveAppBar />
+			</nav>
+			<div className="wrapping">
+				<SearchBar />
+				<Carousel links={links} />
+				<Grid2 container spacing={2}>
+					{DUMMY.map((item) => (
+						<Grid2 xs={12} md={3}>
+							<MediaCard {...item} />
+						</Grid2>
+					))}
+				</Grid2>
+			</div>
+		</>
+	);
 }
 
 export default App;
--- nettube-frontend/src/components/Carousel.tsx
+++ nettube-frontend/src/components/Carousel.tsx
@@ -1,26 +1,31 @@
-import Flickity from 'react-flickity-component'
-import { SelectedVideos } from '../App'
-import '../styles/flickity.css'
+import Flickity from "react-flickity-component";
+import "../styles/flickity.css";
 // https://github.com/yaodingyd/react-flickity-component
 
-
 const flickityOptions = {
-    initialIndex: 1
+	initialIndex: 1,
+};
+
+type CarouselProps = {
+  links:string[]
 }
+
 //NIE UMIEM PRZEKAZYWAĆ ŚCIEŻEK DO OBRAZKÓW HALP IM IN DANGER
-export default function Carousel({videos}: SelectedVideos) {
-  return (
-    <Flickity
-      className={'carousel'} // default ''
-      elementType={'div'} // default 'div'
-      options={flickityOptions} // takes flickity options {}
-      disableImagesLoaded={false} // default false
-      reloadOnUpdate // default false
-      static // default false
-    >
-    {
-    videos.map(video=> <img src={video}/>)
-    }
-    </Flickity>
-  )
+const Carousel = ({links}: CarouselProps) => {
+	return (
+		<Flickity
+			className={"carousel"} // default ''
+			elementType={"div"} // default 'div'
+			options={flickityOptions} // takes flickity options {}
+			disableImagesLoaded={false} // default false
+			reloadOnUpdate // default false
+			static // default false
+		>
+			{links.map((link) => (
+				<img src={link} />
+			))}
+		</Flickity>
+	);
 }
+
+export default Carousel;
