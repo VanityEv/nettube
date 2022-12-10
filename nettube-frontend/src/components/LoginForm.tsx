7121a81a071395455544ad7705bbf6037277b0a4
Jakub Filipowski <filipowski.kuba@gmail.com>
2022-12-10 14:32:34 +0100

    fix(nettube): fixed user login and singup. TODO: token

--- nettube-frontend/src/components/LoginForm.tsx
+++ nettube-frontend/src/components/LoginForm.tsx
@@ -1,13 +1,5 @@
 import { useState } from "react";
-import {
-  Link,
-  Box,
-  Typography,
-  Container,
-  CssBaseline,
-  TextField,
-  Button,
-} from "@mui/material";
+import { Link, Box, Typography, Container, CssBaseline, TextField, Button } from "@mui/material";
 import { ThemeProvider, useTheme } from "@mui/material/styles";
 import { Stack } from "@mui/material";
 import { useAppDispatch } from "../hooks/reduxHooks";
@@ -22,91 +14,79 @@ import { useNavigate } from "react-router-dom";
  */
 
 function SignIn() {
-  const [isUser, setIsUser] = useState(true);
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const theme = useTheme();
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const userCredentials: UserCredentials = {
-      username: data.get("username") as string,
-      password: data.get("password") as string,
-    };
-    dispatch(userLogin(userCredentials));
-    localStorage.getItem("userToken") === null ||
-    localStorage.getItem("userToken") === "undefined"
-      ? setIsUser(false)
-      : navigate("/");
-  };
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="desktop">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <img src="logo.svg" width="150px" height="auto" />
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3, pb: 2 }}
-          >
-            <Stack spacing={3}>
-              {!isUser ? (
-                <Box>
-                  <Typography sx={{ color: "red" }}>
-                    There is no user associated with given credentials!
-                  </Typography>
-                </Box>
-              ) : null}
-              <TextField
-                aria-label="username-field"
-                name="username"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                autoFocus
-              />
-              <TextField
-                aria-label="password-field"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="new-password"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Link href="/Rescue" variant="body1">
-                Forgot password?
-              </Link>
-              <Link href="/Register" variant="body1">
-                Not a member? Register now!
-              </Link>
-            </Stack>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
+	const [isUser, setIsUser] = useState(true);
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
+	const theme = useTheme();
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		const userCredentials: UserCredentials = {
+			username: data.get("username") as string,
+			password: data.get("password") as string,
+		};
+		dispatch(userLogin(userCredentials));
+		localStorage.getItem("userToken") === null ||
+		localStorage.getItem("userToken") === "undefined"
+			? setIsUser(false)
+			: navigate("/");
+	};
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="desktop">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<img src="logo.svg" width="150px" height="auto" />
+					<Typography component="h1" variant="h5">
+						Sign in
+					</Typography>
+					<Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3, pb: 2 }}>
+						<Stack spacing={3}>
+							<TextField
+								aria-label="username-field"
+								name="username"
+								required
+								fullWidth
+								id="username"
+								label="Username"
+								autoFocus
+							/>
+							<TextField
+								aria-label="password-field"
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+								autoComplete="new-password"
+							/>
+							<Button
+								type="submit"
+								fullWidth
+								variant="contained"
+								sx={{ mt: 3, mb: 2 }}
+							>
+								Sign In
+							</Button>
+							<Link href="/Rescue" variant="body1">
+								Forgot password?
+							</Link>
+							<Link href="/Register" variant="body1">
+								Not a member? Register now!
+							</Link>
+						</Stack>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
 }
 export default SignIn;
