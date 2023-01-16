635d1b6e2c28a2b1970b2adf2c8f9510190d39c0
VanityEv <106479438+VanityEv@users.noreply.github.com>
2023-01-16 19:58:59 +0100

    formatting query

--- nettube-backend/services/db.js
+++ nettube-backend/services/db.js
@@ -1,17 +1,22 @@
-import mysql from "mysql";
-import config from "../config/config.js";
+import mysql from 'mysql';
+import config from '../config/config.js';
+
+const formatQuery = sql => {
+  return sql.replaceAll('"', "'");
+};
 
 const query = async (sql, callback) => {
-	const connection = mysql.createConnection(config.db);
-	connection.connect((conErr) => {
-		if (conErr) throw conErr;
-		console.log(`Connection state: ${connection.state}`);
-		connection.query(sql, (queryErr, res) => {
-			if (queryErr) throw queryErr;
-			connection.end();
-			callback(res);
-		});
-	});
+  const formattedQuery = formatQuery(sql);
+  const connection = mysql.createConnection(config.db);
+  connection.connect(conErr => {
+    if (conErr) throw conErr;
+    console.log(`Connection state: ${connection.state}`);
+    connection.query(formattedQuery, (queryErr, res) => {
+      if (queryErr) throw queryErr;
+      connection.end();
+      callback(res);
+    });
+  });
 };
 
 export default query;
